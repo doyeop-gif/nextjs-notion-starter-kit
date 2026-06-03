5906e603348acbf5ba44414019a1c33ac3dc9286
doyeop-gif <doyeop@g.kmou.ac.kr>
2026-06-03 14:25:26 +0900

    수정2

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '37416f5084c180878131c33b5bbbb587,
+  rootNotionPageId: '37416f5084c180878131c33b5bbbb587',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
